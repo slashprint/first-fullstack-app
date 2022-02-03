1573722c44834280209dee22635c39174b714589
slashprint <hello@slashprint.io>
2022-02-03 11:17:01 +0900

    init client

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const { Article } = require('./api')
 const app = express()
 const PORT = 3000
 
 // req.body - POST 에서 들어온 payload를 받을 수 있다.
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
